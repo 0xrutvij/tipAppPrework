f95c6805f34963542dceb4671e2b0d025a8a7cf1
0xrutvij <54966871+0xrutvij@users.noreply.github.com>
2022-01-10 06:13:24 -0600

    Add Swiftlint for Swift convention compliance.

--- Prework/ViewController.swift
+++ Prework/ViewController.swift
@@ -16,55 +16,49 @@ class ViewController: UIViewController {
     @IBOutlet weak var tipSlider: UISlider!
     @IBOutlet weak var tipPercentLabel: UILabel!
 
-    
     @IBAction func otherTipPercent(_ sender: Any) {
-        
+
         let tipPercentIdx = tipControl.selectedSegmentIndex
         let tipPercentages = [15, 18, 20]
-        
+
         if tipPercentIdx == 3 {
             tipSlider.isEnabled = true
-        }
-        else {
+        } else {
             tipSlider.isEnabled = false
             tipSlider.value = Float(tipPercentages[tipPercentIdx])
             tipValueChanged(sender)
         }
-        
+
     }
-    
+
     @IBAction func tipValueChanged(_ sender: Any) {
         defer {
             tipPercentLabel.text = String(format: "%d%%", Int(tipSlider.value))
         }
         calcTip(sender)
-        
+
     }
-    
-    
+
     @IBAction func calcTip(_ sender: Any) {
         let billAmt = Float(billAmountTextField.text!) ?? 0
-        
+
         let sliderPercent = Int(tipSlider.value)
-        
+
         let tip = billAmt * Float(sliderPercent) / 100.0
-        
+
         let total = billAmt + tip
-        
+
         tipAmountLabel.text = String(format: "$%.2f", tip)
-        
+
         totalLabel.text = String(format: "$%.2f", total)
     }
-    
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         billAmountTextField.keyboardType = .decimalPad
         billAmountTextField.becomeFirstResponder()
-        
+
     }
 
-
 }
-
